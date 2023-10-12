ec483641cd7a7d024f63c4a05a580c86f6d3bd31
Julia Oskilko <yulia.osk14@gmail.com>
2023-10-12 11:00:14 +0300

    creating note in progress

--- app/src/main/java/com/josk/notes/HomeFragment.kt
+++ app/src/main/java/com/josk/notes/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 
 class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +28,10 @@ class HomeFragment : Fragment() {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
